ec0b68fb90696c5d2445959c3812e22f41700fd1
Alex <ajtate@colostate.edu>
2021-01-23 08:55:30 -0700

    Added a comment to setup_test.sh to clarify the code which interrupts the installation after an error is encountered.

--- tests/setup_test.sh
+++ tests/setup_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+eval "$(conda shell.bash hook)"
 
 # This script creates a conda environment for every version of Python
 # that is listed in versions array. It then installs aquatx in each
 # environment and reports if there was an error during installation.
 
 versions=( 3.7 3.8 3.9 )
-eval "$(conda shell.bash hook)"
 
 for v in "${versions[@]}"; do
   echo "Testing v$v"
@@ -16,6 +16,8 @@ for v in "${versions[@]}"; do
   conda env update -f ../environment.yml -n aquatx-srna-$v 2>&1 | while read line; do
     if [[ $line == "Found conflicts!"* || $line == "UnsatisfiableError"* ]]; then
       echo "Failed env update."
+      # Send SIGINT (CTRL+C) to conda for graceful exit from env update
+      # This avoids corrupting test environments which in some cases would break this script
       pid=$(ps -e -o pid,command | grep 'environment.yml -n aquatx-srna' | awk 'NR==1{print $1}')
       kill -INT $pid
     fi
