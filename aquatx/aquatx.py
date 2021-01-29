680ebf31ec022f08878e268164af2a1cebcd67be
Alex <ajtate@colostate.edu>
2021-01-29 15:10:40 -0700

    Bugfixes and improved some comments

--- aquatx/aquatx.py
+++ aquatx/aquatx.py
@@ -25,13 +25,13 @@ def get_args():
         "workflows that can be used separately or to retrieve template files."
     )
 
-    # Parser for subcommands run, setup-cwl, setup-nextflow, get-template
+    # Parser for subcommands: (run, setup-cwl, get-template, etc.)
     subparsers = parser.add_subparsers(required=True, dest='command')
 
-    # get-template has no additional arguments
+    # Subcommand get-template has no additional arguments
     subparsers.add_parser("get-template")
 
-    # Commands that require a configuration file parameter
+    # Subcommands that require a configuration file argument
     commands_with_configfile = ["run", "setup-cwl", "setup-nextflow"]
     for command in commands_with_configfile:
         subparsers.add_parser(command).add_argument(
@@ -59,20 +59,23 @@ def get_template(aquatx_extras_path):
         'reference_sheet_template.csv'
     ]
 
+    # Copy template files to the current working directory
     for template in template_files:
-        shutil.copyfile(f"{aquatx_extras_path}{template}", f"./{template}")
+        shutil.copyfile(f"{aquatx_extras_path}/{template}", f"{os.getcwd()}/{template}")
 
 
 def setup_cwl(aquatx_cwl_path, config_file):
     print("Creating cwl workflow...")
 
+    # If the word "None" or "none" is supplied, simply copy workflow files. No config file processing.
     if config_file not in ('None', 'none'):
         # Set up the config file
         processed_config_location = aquatx.srna.configuration_setup.setup_config(config_file)
         print("The processed configuration file is located at: " + processed_config_location)
 
+    # Copy the entire cwl directory to the current working directory
+    shutil.copytree(aquatx_cwl_path, os.getcwd() + "/cwl/")
     print("The workflow and files are under: cwl/tools/ and cwl/workflows/")
-    shutil.copytree(aquatx_cwl_path, './cwl')
 
 
 def setup_nextflow(config_file):
