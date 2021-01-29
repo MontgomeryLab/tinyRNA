b348c142a54d15a6995750ad3dd53a6666d60bed
Alex <ajtate@colostate.edu>
2021-01-29 15:19:07 -0700

    Further solidifying tests. Unit tests for aquatx.py are nearly complete for evaluation within package. Next will be unit tests for the post-installation environment.

--- tests/unit_tests_aquatx.py
+++ tests/unit_tests_aquatx.py
@@ -1,72 +1,139 @@
 #!/usr/bin/env python
 """ Testing module for aquatx.py """
 
+import unit_test_helpers as helpers
 import aquatx.aquatx as aquatx
 import contextlib
+import subprocess
 import unittest
-import filecmp
+import psutil
 import shutil
+import json
 import io
 import os
 
 from pkg_resources import resource_filename
 
-global min_python_version
-min_python_version = 3.7
+"""
 
+======== PRE-INSTALL TESTS ========
+These tests interface directly with the package Python code
+located within the source directory.
+
+"""
 class test_aquatx_preinstall(unittest.TestCase):
-
     def setUp(self):
-        self.f = io.StringIO()
-        self.aquatx_cwl_path = resource_filename('aquatx', 'cwl/')
-        self.aquatx_extras_path = resource_filename('aquatx', 'extras/')
+        self.stdout_capture = io.StringIO()
+        self.aquatx_cwl_path = '../aquatx/cwl'
+        self.aquatx_extras_path = '../aquatx/extras'
+        self.config_file = './testdata/run_config_template.yml'
+        self.expected_cwl_dir_tree = {
+            'cwl': {
+                'files': [],
+                'tools': {
+                    'files': [
+                        'aquatx-deseq.cwl', 'bowtie.cwl',
+                        'aquatx-collapse.cwl', 'bowtie-build.cwl',
+                        'aquatx-count.cwl', 'aquatx-merge.cwl', 'fastp.cwl'
+                    ]
+                },
+                'workflows': {
+                    'files': ['aquatx_wf.cwl']
+                }
+            }
+        }
+
 
     """
-    Testing that aquatx.get_template copies the correct files to the current directory.
+    Testing that aquatx.get_template copies the correct files 
+    to the current directory.
     """
     def test_get_template(self):
-        template_files = ['run_config_template.yml', 'sample_sheet_template.csv', 'reference_sheet_template.csv']
-        before_count = len(os.listdir('.'))
-        aquatx.get_template(self.aquatx_extras_path)
-        self.assertEquals(len(os.listdir('.')) - before_count, 3, "Abnormal number of template files. Expected 3.")
+        template_files = [
+            'run_config_template.yml', 'sample_sheet_template.csv',
+            'reference_sheet_template.csv'
+        ]
 
-        for file in template_files:
-            self.assertTrue(os.path.isfile(file), "An expected template file wasn't copied.")
-            os.remove(file)
+        try:
+            before_count = len(os.listdir('.'))
+            aquatx.get_template(self.aquatx_extras_path)
+            self.assertEqual(
+                len(os.listdir('.')) - before_count, 3,
+                "Abnormal number of template files. Expected 3.")
+
+            for file in template_files:
+                self.assertTrue(os.path.isfile(file),
+                                "An expected template file wasn't copied.")
+                os.remove(file)
+        finally:
+            for file in template_files: os.remove(file)
+
 
     """
-    Testing that aquatx.setup_cwl with --config None/none copies workflow files without mentioning the config file
+    Testing that aquatx.setup_cwl with --config None/none 
+    copies workflow files without mentioning the config file
     """
     def test_setup_cwl_noconfig(self):
         no_config = ['None', 'none']
         for config in no_config:
-            with contextlib.redirect_stdout(self.f):
-                aquatx.setup_cwl(self.aquatx_cwl_path, config)
-                filecmp.dircmp(self.aquatx_extras_path, './cwl')
+            try:
+                with contextlib.redirect_stdout(self.stdout_capture):
+                    aquatx.setup_cwl(self.aquatx_cwl_path, config)
 
-            self.assertTrue(os.path.isdir('cwl/tools') and os.path.isdir('cwl/workflows'), "Expected CWL outputs don't exist.")
-            self.assertNotIn("configuration", self.f.getvalue(), "Setup mentioned configfile when None was provided")
-            self.assertEquals([f for f in os.walk('./cwl/')], [f for f in os.walk(self.aquatx_cwl_path)])
-            shutil.rmtree('./cwl')
+                self.assertNotIn(
+                    "configuration", self.stdout_capture.getvalue(),
+                    "Setup mentioned configfile when None was provided")
+                self.assertEqual(helpers.get_dir_tree('./cwl'),
+                                  self.expected_cwl_dir_tree,
+                                 "The expected local cwl directory tree was not found")
+            finally:
+                if os.path.isdir('./cwl'): shutil.rmtree('./cwl')
+
 
     """
-    Testing that aquatx.setup
+    Testing that aquatx.setup_cwl WITH config file properly
+    processes the input configfile, then copies workflow files.
     """
     def test_setup_cwl_withconfig(self):
-        config_file = './testdata/run_config_template.yml'
-        aquatx.setup_cwl(self.aquatx_cwl_path, config_file)
-        self.assertTrue(os.path.isdir('cwl/tools') and os.path.isdir('cwl/workflows'))
+        try:
+            with contextlib.redirect_stdout(self.stdout_capture):
+                aquatx.setup_cwl(self.aquatx_cwl_path, self.config_file)
+
+            self.assertEqual(helpers.get_dir_tree('./cwl'),
+                              self.expected_cwl_dir_tree)
+            self.assertIn("The processed configuration file is located at: ",
+                          self.stdout_capture.getvalue(),
+                          "Setup failed to mention the location of the processed config file.")
+        finally:
+            if os.path.isdir('./cwl'): shutil.rmtree('./cwl')
 
 
+    """
+    Test that aquatx.run spawns a cwltool subprocess
+    """
+    def test_run(self):
+        try:
+            with contextlib.redirect_stdout(self.stdout_capture):
+                aquatx.run(self.aquatx_cwl_path, self.config_file)
+                self_pid = psutil.Process(os.getpid())
+                self.assertTrue('cwltool' in self_pid.children)
+        finally:
+            print(self.stdout_capture.getvalue())
+
+"""
+
+======== POST-INSTALL TESTS ========
+These tests interface with the installed aquatx package via 
+the command line
+
+"""
 class test_aquatx_postinstall(unittest.TestCase):
-
     def setUp(self):
-        os.system(f"conda env create -n aquatx-test python={min_python_version} --yes > /dev/null")
+        self.MIN_PYTHON = 3.7
+        os.system(f"conda env create -n aquatx-test python={self.MIN_PYTHON} --yes > /dev/null")
         os.system(f"conda env update -n aquatx-text -f ../environment.yml > /dev/null")
 
     #def test_get_template(self):
-
-
 
 
 if __name__ == '__main__':
