6e723307050b8f735777efaa91539233ce66648b
AlexTate <ajtate@colostate.edu>
2021-08-01 18:21:19 -0700

    Bugfix in load_samples(). An input validation step was failing if user had gzipped fastq files in their samples sheet. I could have sworn this issue had already been fixed...

--- aquatx/srna/counter/counter.py
+++ aquatx/srna/counter/counter.py
@@ -60,7 +60,7 @@ def load_samples(samples_csv: str) -> list:
         file_ext = os.path.splitext(csv_row_file)[1].lower()
 
         # If the sample file has a fastq(.gz) extension, infer the name of its pipeline-produced .sam file
-        if file_ext in [".fastq", ".fastq.gz"]:
+        if file_ext in [".fastq", ".gz"]:
             # Fix relative paths to be relative to sample_csv's path, rather than relative to cwd
             csv_row_file = os.path.basename(csv_row_file) if is_pipeline else from_here(samples_csv, csv_row_file)
             csv_row_file = os.path.splitext(csv_row_file)[0] + "_aligned_seqs.sam"
@@ -68,7 +68,7 @@ def load_samples(samples_csv: str) -> list:
             if not os.path.isabs(csv_row_file):
                 raise ValueError("The following file must be expressed as an absolute path:\n%s" % (csv_row_file,))
         else:
-            raise ValueError("The filenames defined in your samples CSV file must have a .fastq or .sam extension.\n"
+            raise ValueError("The filenames defined in your Samples Sheet must have a .fastq(.gz) or .sam extension.\n"
                              "The following filename contained neither:\n%s" % (csv_row_file,))
         return csv_row_file
 
