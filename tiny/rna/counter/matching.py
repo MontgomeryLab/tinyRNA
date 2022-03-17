3f474291fb1ff02aae59325dca65d9d343a7ab83
Alex Tate <0xalextate@gmail.com>
2022-03-17 10:10:47 -0700

    - Major correction to the semantics of 5'/3' anchored interval selectors. These classes now have a diagram comment to better explain their meaning. - Improved inheritance pattern for interval selectors. The base class now handles slot assignment (start/end position) for all selectors. Better expression and cleaner code. The base class also handles hashability so that match tuples can be stored in GenomicArraysOfSets - The Wildcard selector now uses slots in order to conform to the inheritence requirements for slots (IntervalPartial match inherits from it) - Repr strings have been clarified and now more accurately represent half-open intervals

--- tiny/rna/counter/matching.py
+++ tiny/rna/counter/matching.py
@@ -5,10 +5,11 @@ import HTSeq
 
 
 class Wildcard:
+    __slots__ = ()
+
     @staticmethod
     def __contains__(_): return True
-    def __repr__(_): return "all"
-
+    def __repr__(_): return "<all>"
 
 class StrandMatch:
     """Evaluates BOTH the alignment's strand and the feature's strand for a match
@@ -66,7 +67,29 @@ class NumericalMatch(frozenset):
         return super().__new__(cls, lengths)
 
 
-class IntervalSelector: __slots__ = ()
+class IntervalSelector:
+    __slots__ = ("start", "end")
+
+    """IntervalSelector classes use __slots__ rather than class attributes to
+    reduce memory footprint. As a result, each instance requires only 128 bytes
+    (compare to: 184 bytes empty class without slots, or 248 bytes empty dict).
+    Instances of these classes may be numerous depending on the user's GFF file;
+    one IntervalSelector is created for each unique match definition, for each
+    interval associated with each retained feature."""
+
+    def __init__(self, iv: HTSeq.GenomicInterval):
+        """Descendents only need to know the start and end coordinates of the target feature"""
+        self.start = iv.start
+        self.end = iv.end
+
+    def __hash__(self):
+        """Descendents must be hashable in order to be stored in a GenomicArrayOfSets"""
+        return self.start ^ self.end
+
+    def __eq__(self, other):
+        return self.__class__.__name__ == other.__class__.__name__ and \
+               self.start == other.start and \
+               self.end == other.end
 
 
 class IntervalPartialMatch(Wildcard, IntervalSelector):
@@ -76,65 +99,106 @@ class IntervalPartialMatch(Wildcard, IntervalSelector):
     at least one base, no further evaluation is required when this filter
     is used by FeatureSelector.choose()"""
 
-    __slots__ = ("start", "end")
-
     def __init__(self, iv: HTSeq.GenomicInterval):
-        self.start = iv.start
-        self.end = iv.end
+        super().__init__(iv)
 
     def __repr__(self):
-        return f"Any overlap {self.start}:{self.end}"
+        return f"<Any overlap [{self.start}, {self.end})>"
 
 
 class IntervalFullMatch(IntervalSelector):
-    __slots__ = ("start", "end")
 
     def __init__(self, iv: HTSeq.GenomicInterval):
-        self.start = iv.start
-        self.end = iv.end
+        super().__init__(iv)
 
     def __contains__(self, alignment):
         return self.start <= alignment['start'] and alignment['end'] <= self.end
 
     def __repr__(self):
-        return f"Between {self.start}:{self.end}"
+        return f"<Between [{self.start}, {self.end})>"
 
 
 class IntervalExactMatch(IntervalSelector):
-    __slots__ = ("start", "end")
 
     def __init__(self, iv: HTSeq.GenomicInterval):
-        self.start = iv.start
-        self.end = iv.end
+        super().__init__(iv)
 
     def __contains__(self, alignment):
         return self.start == alignment['start'] and alignment['end'] == self.end
 
     def __repr__(self):
-        return f"Exactly {self.start}:{self.end}"
+        return f"<Exactly [{self.start}, {self.end})>"
 
 
 class Interval5pMatch(IntervalSelector):
-    __slots__ = ("start")
+    """Evaluates whether an alignment's 5' end is anchored to the corresponding terminus of the feature"""
 
     def __init__(self, iv: HTSeq.GenomicInterval):
-        self.start = iv.start
+        super().__init__(iv)
 
-    def __contains__(self, alignment):
-        return self.start == alignment['start']
+    def __contains__(self, alignment: dict):
+        """The following diagram demonstrates 5' anchored matching semantics.
+        Each "(no) match" label applies to BOTH feat_A AND feat_B.
+
+                 No match  |   -------->|
+                    Match  |------------|-->
+                    Match  |----------->|
+                    Match  |-------->   |
+        (+) 5' ------------|==feat_A===>|-----------> 3'
+
+        (-) 3' <-----------|<===feat_B==|------------ 5'
+                           |   <--------|  Match
+                           |<-----------|  Match
+                        <--|------------|  Match
+                           |<--------   |  No match
+        Args:
+            alignment: An alignment dictionary containing strand, start, and end
+
+        Returns: True if the alignment's 5' end is anchored to the strand-appropriate
+            terminus of this feature's interval.
+        """
+
+        if alignment['strand'] is '+':
+            return alignment['start'] == self.start
+        else:
+            return alignment['end'] == self.end
 
     def __repr__(self):
-        return f"5' anchored to {self.start}"
+        return f"<5' anchored to {self.start} on (+) / {self.end} on (-)>"
 
 
 class Interval3pMatch(IntervalSelector):
-    __slots__ = ("end")
+    """Evaluates whether an alignment's 5' end is anchored to the corresponding terminus of the feature"""
 
     def __init__(self, iv: HTSeq.GenomicInterval):
-        self.end = iv.end
+        super().__init__(iv)
 
     def __contains__(self, alignment):
-        return self.end == alignment['end']
+        """The following diagram demonstrates 3' anchored matching semantics.
+        Each "(no) match" label applies to BOTH feat_A AND feat_B.
+
+               No match    |-------->   |
+                  Match  --|----------->|
+                  Match    |----------->|
+                  Match    |   -------->|
+        (+) 5' ------------|==feat_A===>|-----------> 3'
+
+        (-) 3' <-----------|<===feat_B==|------------ 5'
+                           |<--------   |    Match
+                           |<-----------|    Match
+                           |<-----------|--  Match
+                           |   <--------|    No match
+        Args:
+            alignment: An alignment dictionary containing strand, start, and end
+
+        Returns: True if the alignment's 3' end is anchored to the strand-appropriate
+            terminus of this feature's interval.
+        """
+
+        if alignment["strand"] is '+':
+            return alignment['end'] == self.end
+        else:
+            return alignment['start'] == self.start
 
     def __repr__(self):
-        return f"3' anchored to {self.end}"
+        return f"<3' anchored to {self.end} on (+) / {self.start} on (-)>"
