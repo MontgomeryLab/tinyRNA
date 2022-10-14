05856b02e3cb5ba652646d5685bf6e9c25d92fac
Alex Tate <0xalextate@gmail.com>
2022-10-13 17:14:15 -0700

    Configuration.setup_ebwt_idx() has been updated for the new bowtie-build activation procedure. If the user defines ebwt but index files can't be found, and they also provided their reference genome files, then the pipeline will automatically rebuild the indexes and update the Paths Sheet at the end of the end-to-end run.
    setup_ebwt_idx() has also been significantly refactored and cleaned up. It has been bugging me for a long time and it feels good to see it in better shape.

--- tiny/rna/configuration.py
+++ tiny/rna/configuration.py
@@ -279,38 +279,65 @@ class Configuration(ConfigBase):
         self.templates = resource_filename('tiny', 'templates/')
 
     def setup_ebwt_idx(self):
-        """Bowtie index files and prefix"""
+        """Determines Bowtie index prefix and whether bowtie-build should run"""
 
-        # Determine if bowtie-build should run, and set Bowtie index prefix accordingly
-        bt_index_prefix = self.paths['ebwt']
-        if self['run_bowtie_build'] and not bt_index_prefix:
-            if not self['reference_genome_files']:
-                raise ValueError(f"If {self.basename} contains 'run_bowtie_build: True', you "
-                                 f"need to provide your reference genome files in {self.paths.basename}")
+        # Empty values for ebwt (''/~/None) trigger bowtie-build
+        self['run_bowtie_build'] = not bool(self.paths['ebwt'])
 
-            # Outputs are saved in {run_directory}/bowtie-build, within which prefix is first genome file's basename
-            first_genome_file = self.paths.from_here(self['reference_genome_files'][0]['path'])
-            bt_index_prefix = self.prefix(os.path.join(
-                self['run_directory'], "bowtie-build", os.path.basename(first_genome_file))
-            )
-
-            self['ebwt'] = self.paths['ebwt'] = bt_index_prefix
+        if self['run_bowtie_build']:
+            # Set the prefix to the run directory outputs. This is necessary
+            # because workflow requires bt_index_files to be a populated list.
+            prefix = self.get_ebwt_prefix()
         else:
-            # bowtie-build should only run if 'run_bowtie_build' is True AND ebwt (index prefix) is undefined
-            self['run_bowtie_build'] = False
-            bt_index_prefix = self.paths.from_here(bt_index_prefix)
+            prefix = self.paths.from_here(self.paths['ebwt'])
 
-        # Bowtie index files
-        try:
-            self['bt_index_files'] = [self.cwl_file(bt_index_prefix + postfix, verify=(not self['run_bowtie_build']))
-                            for postfix in ['.1.ebwt', '.2.ebwt', '.3.ebwt', '.4.ebwt', '.rev.1.ebwt', '.rev.2.ebwt']]
-        except FileNotFoundError as e:
-            sys.exit("The following file could not be found from the Bowtie index prefix defined in your Paths File:\n"
-                     "%s" % (e.filename,))
+        # verify_bowtie_build_outputs() will check if these end up being long indexes
+        self['bt_index_files'] = self.get_bt_index_files(prefix)
 
         # When CWL copies bt_index_filex for the bowtie.cwl InitialWorkDirRequirement, it does not
         # preserve the prefix path. What the workflow "sees" is the ebwt files at working dir root
-        self["ebwt"] = os.path.basename(self["ebwt"])
+        self["ebwt"] = os.path.basename(prefix)
+
+    def get_ebwt_prefix(self):
+        """Determines the output prefix path for bowtie indexes that haven't been built yet. The basename
+        of the prefix path is simply the basename of the reference genome sans file extension"""
+
+        genome_files = self['reference_genome_files']
+        if not genome_files:
+            raise ValueError("If your Paths Sheet doesn't have a value for \"ebtw:\", then bowtie indexes "
+                             "will be built, but you'll need to provide your reference genome files under "
+                             '"reference_genome_files:" (also in your Paths Sheet)')
+
+        genome_basename = os.path.basename(genome_files[0]['path'])
+        return self.prefix(os.path.join( # prefix path:
+            self['run_directory'], self['dir_name_bt_build'], genome_basename
+        ))
+
+    def get_bt_index_files(self, prefix):
+        """Builds the list of expected bowtie index files from the ebwt prefix. If an index file
+        doesn't exist then they will be automatically rebuilt from the user's reference genomes.
+        File existence isn't checked if bowtie-build is already scheduled for this run."""
+
+        try:
+            verify_file_paths = not bool(self['run_bowtie_build'])
+            ext = "ebwt"
+
+            return [
+                self.cwl_file(f"{prefix}.{subext}.{ext}", verify=verify_file_paths)
+                for subext in ['1', '2', '3', '4', 'rev.1', 'rev.2']
+            ]
+        except FileNotFoundError as e:
+            problem = "The following Bowtie index file couldn't be found:\n\t%s\n\n" % (e.filename,)
+            rebuild = "Indexes will be built from your reference genome files during this run."
+            userfix = "Please either correct your ebwt prefix or add reference genomes in the Paths File."
+
+            if self['reference_genome_files']:
+                print(problem + rebuild, file=sys.stderr)
+                new_prefix = self.get_ebwt_prefix()
+                self['run_bowtie_build'] = True
+                return self.get_bt_index_files(new_prefix)
+            else:
+                sys.exit(problem + userfix)
 
     def validate_inputs(self):
         """For now, only GFF files are validated here"""
