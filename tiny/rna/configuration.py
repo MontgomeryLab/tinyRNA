7df7289d488336f49b46ff8d3371d80be6ee5d6e
Alex Tate <0xalextate@gmail.com>
2023-03-07 13:39:13 -0800

    CSVReader now skips empty rows, including rows whose cells contain only whitespace values

--- tiny/rna/configuration.py
+++ tiny/rna/configuration.py
@@ -5,6 +5,7 @@ import errno
 import time
 import sys
 import csv
+import re
 import os
 
 from pkg_resources import resource_filename
@@ -799,8 +800,13 @@ class CSVReader(csv.DictReader):
             # and it doesn't make sense to infer column identity
             self.validate_csv_header(header)
 
+            # For skipping rows that are empty or all whitespace cells
+            non_ws_cell = lambda cell: re.sub(r'^\s+$', '', cell)
+            empty_row = lambda row: not any(non_ws_cell(x) for x in row.values())
+
             for row in self:
                 self.row_num += 1
+                if empty_row(row): continue
                 yield row
 
     def replace_excel_ellipses(self):
