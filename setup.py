12c53bb456e4625d169c942d45819ccde564f22a
Alex Tate <0xalextate@gmail.com>
2023-06-01 23:25:38 -0700

    Version bump to 1.5.0 and Python 3.10 in setup.py

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ URL = 'https://github.com/MontgomeryLab/tinyrna/'
 EMAIL = 'ajtate@colostate.edu'
 AUTHOR = 'Kristen Brown, Alex Tate'
 PLATFORM = 'Unix'
-REQUIRES_PYTHON = '>=3.9.0'
-VERSION = '1.4.0'
+REQUIRES_PYTHON = '>=3.10.0'
+VERSION = '1.5.0'
 REQUIRED = []  # Required packages are installed via Conda's environment.yml
 
 
