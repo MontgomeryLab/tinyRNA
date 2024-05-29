95200e38a15f8f121434011925590ed69e9acaf3
Alex Tate <0xalextate@gmail.com>
2024-05-28 20:46:00 -0700

    AlignmentIter now returns the MD tag in alignment dictionaries when detailed_mismatch is True.
    Other major changes to AlignmentIter:
    - Soft clipping is now honored in the Seq, Start, and End alignment dictionary keys.
    - The constructor arg has_nm has been changed to expected_tags, which is just a tuple of the tags found in the first alignment. At construction this is checked for the NM tag, and if the new constructor arg detailed_mismatch is True, the MD tag is also checked. It's an error if these tags are missing but expected.
    - CIGAR parsing for edit distance is no longer supported until the method can be rewritten. The current implementation does not properly disambiguate M operations. For this reason the NM tag is now (temporarily) required.
    - Unmapped alignments are no longer skipped by recursively calling _next_alignment(). Python's max recursion depth is actually quite shallow, so a SAM/BAM file with 1000+ unmapped alignments would produce a RecursionError.

--- tiny/rna/counter/parsing/alignments.pyx
+++ tiny/rna/counter/parsing/alignments.pyx
@@ -4,7 +4,7 @@
 cimport cython
 from pysam.libcalignmentfile cimport AlignmentFile, AlignedSegment
 from pysam.libcalignedsegment cimport pysam_bam_get_qname
-from pysam.libchtslib cimport bam_aux_get, bam_aux2i, BAM_FUNMAP, BAM_FREVERSE
+from pysam.libchtslib cimport bam_aux_get, bam_aux2i, bam_aux2Z, BAM_FUNMAP, BAM_FREVERSE
 
 from typing import Callable
 
@@ -16,16 +16,29 @@ cdef class AlignmentIter:
     cdef tuple references
     cdef object dc_callback
     cdef list dc_queue
+    cdef bint detailed_mismatch
     cdef bint decollapse
-    cdef bint has_nm
 
-    def __init__(self, pysam_reader: AlignmentFile, has_nm: bool, dc_callback: Callable, dc_queue: list):
+
+    def __init__(
+            self,
+            pysam_reader: AlignmentFile,
+            expected_tags: tuple,
+            dc_callback: Callable,
+            dc_queue: list,
+            detailed_mismatch: bint
+    ):
         self.reader = pysam_reader
         self.references = pysam_reader.header.references
         self.decollapse = dc_callback is not None
         self.dc_callback = dc_callback
         self.dc_queue = dc_queue
-        self.has_nm = has_nm
+        self.detailed_mismatch = detailed_mismatch
+
+        if "NM" not in expected_tags:
+            raise NotImplementedError("Alignments must include the NM tag.")
+        if "MD" not in expected_tags and detailed_mismatch is True:
+            raise ValueError(f"Alignments must include the MD tag for detailed mismatches.")
 
     def __iter__(self):
         return self
@@ -37,25 +50,25 @@ cdef class AlignmentIter:
         cdef:
             AlignedSegment aln = next(self.reader)                  # Equivalent Python API calls:
             int flag = aln._delegate.core.flag                          # aln.flag
-            str seq = aln.query_sequence
-            int start = aln._delegate.core.pos                          # aln.reference_start
-            int length = aln._delegate.core.l_qseq                      # aln.query_length
-            bint strand = (flag & BAM_FREVERSE) == 0                    # aln.is_forward
-            str nt5 = AlignmentIter._get_nt5(seq, strand)
-            str name = pysam_bam_get_qname(aln._delegate).decode()      # aln.query_name
+            int start, length
+            str seq, nt5, name
+            bint strand
 
-        if (flag & BAM_FUNMAP) != 0:                                    # aln.is_unmapped
-            return self._next_alignment()
+        # Skip unmapped alignments
+        while flag & BAM_FUNMAP:                                        # aln.is_unmapped
+            aln = next(self.reader)
+            flag = aln._delegate.core.flag
 
-        if self.decollapse:
-            self._append_to_dc_queue(aln)
+        # Extract relevant info from Pysam's AlignedSegment
+        seq = aln.query_alignment_sequence
+        start = aln._delegate.core.pos + aln.query_alignment_start      # aln.reference_start
+        strand = (flag & BAM_FREVERSE) == 0                             # aln.is_forward
+        nt5 = AlignmentIter._get_nt5(seq, strand)
+        name = pysam_bam_get_qname(aln._delegate).decode()              # aln.query_name
+        mismatches = AlignmentIter._get_mismatches_from_nm(aln)
+        length = len(seq)
 
-        if self.has_nm:
-            mismatches = AlignmentIter._get_mismatches_from_nm(aln)     # aln.get_tag("NM")
-        else:
-            mismatches = AlignmentIter._get_mismatches_from_cigar(aln)
-
-        return {
+        alignment = {
             "Name": name,
             "Length": length,
             "Seq": seq,
@@ -67,16 +80,32 @@ cdef class AlignmentIter:
             "NM": mismatches
         }
 
+        if self.detailed_mismatch:
+            alignment["MD"] = AlignmentIter._get_md_string(aln)
+
+        if self.decollapse:
+            self._append_to_dc_queue(aln)
+
+        return alignment
+
     @staticmethod
-    cdef _get_mismatches_from_nm(AlignedSegment aln):
+    cdef int _get_mismatches_from_nm(AlignedSegment aln):               # aln.get_tag("NM")
         nm = bam_aux_get(aln._delegate, b"NM")
         if nm == NULL: return 0  # Assume missing tag means NM:i:0
         return bam_aux2i(nm)
 
     @staticmethod
-    cdef _get_mismatches_from_cigar(AlignedSegment aln):
-        # Calculate mismatches using the CIGAR string's I, D, and X operations
-        return sum(op_len for op, op_len in aln.cigartuples if op in cigar_mismatch)
+    cdef object _get_mismatches_from_cigar(AlignedSegment aln):
+        # This method doesn't disambiguate the M operator. Revision required.
+        raise NotImplementedError("Mismatches can't be calculated from CIGAR in this version.")
+
+    @staticmethod
+    cdef str _get_md_string(AlignedSegment aln):                        # aln.get_tag("MD")
+        md = bam_aux_get(aln._delegate, b"MD")
+        if md == NULL:
+            raise ValueError("Alignment is missing a required MD tag.")
+
+        return bam_aux2Z(md).decode()
 
     @staticmethod
     cdef str _get_nt5(str seq, bint strand):
